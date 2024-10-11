26d7fc56d80784932552df3afe0ea3008dcfaf82
paldea <paldea@umich.edu>
2024-10-11 19:56:14 -0400

    Working?

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,6 +6,16 @@ set -Eeuo pipefail
 sudo apt update
 sudo apt install python3-pip
 
-#
-# cd ~/local/repository
+sudo pip install --upgrade pip
+
+sudo apt install python3.10-venv
+
+cd ~/local/repository
+
+sudo python3 -m venv env
+source env/bin/activate
+
+pip install jupyterlab
+pip install torch
+pip install transformers
 
