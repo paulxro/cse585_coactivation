10d2f63eb148548ebae65a4e5ba9926911a446c0
paldea <paldea@umich.edu>
2024-10-11 19:15:16 -0400

    Fixed paths?

--- profile.py
+++ profile.py
@@ -4,7 +4,7 @@ import geni.portal as portal
 import geni.rspec.pg as pg
 import os
 
-SCRIPT_DIR = './scripts'
+SCRIPT_DIR = '/local/repository/scripts/'
 
 
 pc = portal.Context()
@@ -13,13 +13,15 @@ request = pc.makeRequestRSpec()
 
 node = request.RawPC("node")
 
+files = [file for file in os.listdir(SCRIPT_DIR)]
+
+for file in files:
+    os.chmod(SCRIPT_DIR + file, 0o0777)
+
 node.addService(
     pg.Execute(shell="sh", command="/local/repository/scripts/startup.sh")
 )
 
-files = [file for file in os.listdir(SCRIPT_DIR)]
 
-for file in files:
-    os.chmod('./scripts/' + file, 0o0777)
 
 pc.printRequestRSpec(request)
