6edeef1a64db8300cf6b9eb362ac0f3340db19c7
paldea <paldea@umich.edu>
2024-10-11 19:05:53 -0400

    Made scripts executable.

--- profile.py
+++ profile.py
@@ -1,14 +1,22 @@
 """Start-up script adapted from https://github.com/emulab/my-profile"""
 
 import geni.portal as portal
-
 import geni.rspec.pg as pg
+import os
+
+SCRIPT_DIR = './scripts'
+
 
 pc = portal.Context()
 
 request = pc.makeRequestRSpec()
 
 node = request.RawPC("node")
+
+files = [file for file in os.listdir(SCRIPT_DIR)]
+
+for file in files:
+    os.chmod(f'./scripts/{file}', 0o0777)
 
 node.addService(
     pg.Execute(shell="sh", command="/local/repository/scripts/startup.sh")
